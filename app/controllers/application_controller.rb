e291c0dd5509bb903c5d4f2fc5deb55a6edc914b
Mathew M <mathewmastromonaco@gmail.com>
2024-09-12 18:42:03 -0400

    user register and login setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
   private
 
   def cart
@@ -21,10 +30,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-  def formatted_currency(price)
-    format('$%.2f', price)
-  end
-  helper_method :formatted_currency
 
   def update_cart(new_cart)
     cookies[:cart] = {
