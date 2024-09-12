fde67636a004f28fc92c6fdb74713db2bbd22b51
Mathew M <mathewmastromonaco@gmail.com>
2024-09-12 13:46:18 -0400

    basic login feature added for admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
@@ -35,6 +36,13 @@ class Admin::ProductsController < ApplicationController
       :image,
       :price
     )
+  end
+
+  def authenticate
+    puts "Username: #{ENV['ADMIN_USERNAME']}, Password: #{ENV['ADMIN_PASSWORD']}"
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 
 end
