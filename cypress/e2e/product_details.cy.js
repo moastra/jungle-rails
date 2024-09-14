dc71229061b14eae962d4aff0b5e0575b18569e0
Mathew M <mathewmastromonaco@gmail.com>
2024-09-13 21:26:37 -0400

    product details test setup with cypress

---
+++ cypress/e2e/product_details.cy.js
@@ -0,0 +1,23 @@
+describe("Home Page", () => {
+  it("should visit the home page", () => {
+    cy.visit("/");
+
+    cy.title().should("include", "Jungle");
+  });
+
+  it("There is products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("should click on the first product on the page", () => {
+    cy.visit("/");
+
+    cy.get("article").first().click();
+
+    cy.url().should("include", "/products");
+
+    cy.get("h1").should("contain", "Scented Blade");
+    cy.get("span").contains("$24.99");
+  });
+});
